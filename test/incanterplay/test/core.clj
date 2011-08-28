62483225c8782dc7a54271c58d7feea10cc6057b
Federico Brubacher <fbrubacher@gmail.com>
2011-08-28 14:59:13 -0300

    add linear-reg test

--- test/incanterplay/test/core.clj
+++ test/incanterplay/test/core.clj
@@ -1,6 +1,8 @@
 (ns incanterplay.test.core
   (:use [hastie-clj.linear-reg] :reload)
-  (:use [clojure.test]))
+  (:use [clojure.test])
+  (:require [incanter.core :as core]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest calculate-weights-test
+  (let [result (list 4.124143489627892 0.4800732928842445 -0.6168481970344016)]
+    (is (= result (core/to-list (calculate-weights))))))
